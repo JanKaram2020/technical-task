3eb2b66ecadee301a2b1226caeb63f4f1aea15f8
jan <jankarm2020@gmail.com>
2025-02-10 18:26:54 +0200

    split data fetching to a hook & handle error case & improve UI

--- app/items-list/items-list.tsx
+++ app/items-list/items-list.tsx
@@ -1,32 +1,74 @@
-import React, { useState, useEffect } from "react";
-function ItemsList() {
-  const [items, setItems] = useState([]);
-  const [loading, setLoading] = useState(true);
+"use client";
+import React, { useState, useEffect, useRef } from "react";
+
+type Todos = Array<{
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}>;
+
+const useTodos = () => {
+  const [items, setItems] = useState<Todos>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const abortControllerRef = useRef<AbortController | null>(null);
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
-      .then((response) => response.json())
-      .then((data) => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      const abortController = new AbortController();
+      abortControllerRef.current = abortController;
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/todos", {
+          signal: abortController.signal,
+        });
+        if (!res.ok) {
+          setError("Failed to fetch todos");
+          return;
+        }
+        const data: Todos = await res.json();
         setItems(data);
+      } catch (e) {
+        if (!(e instanceof DOMException && e.name === "AbortError")) {
+          setError("An error occurred while fetching todos.");
+        }
+      } finally {
         setLoading(false);
-      })
-      .catch((error) => {
-        console.log("Error fetching items:", error);
-        setLoading(false);
-      });
+      }
+    };
+
+    fetchData();
+
+    return () => abortControllerRef.current?.abort();
   }, []);
+
+  return { items, loading, error };
+};
+
+const ItemsList = () => {
+  const { items, loading, error } = useTodos();
+
   return (
     <div>
       <h2>Items List</h2>
-      {loading ? (
+      {error ? (
+        <p>{error}</p>
+      ) : loading ? (
         <p>Loading...</p>
-      ) : (
+      ) : Array.isArray(items) && items.length ? (
         <ul>
-          {items.map((item, index) => (
-            <li key={index}>{item.title}</li>
+          {items.map((item) => (
+            <li key={item.id}>
+              <p>{item.title}</p>
+              <span>{item.completed ? "Completed" : "Pending"}</span>
+            </li>
           ))}
         </ul>
-      )}
+      ) : null}
     </div>
   );
-}
+};
+
 export default ItemsList;
