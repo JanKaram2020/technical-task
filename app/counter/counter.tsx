2b9f136efd637a50f4fdc8ab2a837b066a036e02
jan <jankarm2020@gmail.com>
2025-02-10 18:10:06 +0200

    split counter component to two components to increase performance and remove unnecessary renders

--- app/counter/counter.tsx
+++ app/counter/counter.tsx
@@ -1,20 +1,37 @@
+"use client";
 import React, { useState } from "react";
-function Counter() {
+
+const InnerCounter = () => {
   const [count, setCount] = useState(0);
+  return (
+    <>
+      <h2>Counter: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </>
+  );
+};
+
+const TextInput = () => {
   const [text, setText] = useState("");
+  return (
+    <input
+      type="text"
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+      placeholder="Type something..."
+    />
+  );
+};
+
+function Counter() {
   console.log("Counter component rendered");
   return (
     <div>
-      <h2>Counter: {count}</h2>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <InnerCounter />
       <br />
-      <input
-        type="text"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        placeholder="Type something..."
-      />
+      <TextInput />
     </div>
   );
 }
+
 export default Counter;
